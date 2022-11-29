650f1aadcc0772b4464fda05a79ec471c2a13895
NazarVik <Nazarov_dev@icloud.com>
2022-11-29 16:27:44 +0300

    решил 19 задачу

--- task19/Program.cs
+++ task19/Program.cs
@@ -5,5 +5,23 @@
 // 12821 -> да
 // 23432 -> да
 
+void PolindromCheck(string str)
+{
+    char[] arr = str.ToCharArray();
+    char[] reversedArr = new char[5];
+    int count = 0;
+
+    for(int i = arr.Length -1; i >= 0; i--)
+    {
+        reversedArr[count] = arr[i];
+        count++;
+    }
+
+    if(arr.SequenceEqual(reversedArr)) Console.WriteLine(str + " -> да");
+    else Console.WriteLine(str + " -> нет");
+}
+
+Console.Clear();
 Console.Write("введите пятизначное число = ");
-int number  = int.Parse(Console.ReadLine());
+string number  = Console.ReadLine();
+PolindromCheck(number);
